8ca182f5b0881aaca78ca7fffa9b742d6c8afcb8
Ivan Petrushev <ivanatora@gmail.com>
2020-05-11 18:44:41 +0300

    gis: functions for latlon arguments

--- client-app/src/gis.js
+++ client-app/src/gis.js
@@ -61,6 +61,30 @@ export let gis = {
 
         return [gis.toDeg(λ2), gis.toDeg(φ2)];
     },
+    createCoordLatLon: function (coord, bearing, distance) {
+        /** http://www.movable-type.co.uk/scripts/latlong.html
+         * φ is latitude, λ is longitude,
+         * θ is the bearing (clockwise from north),
+         * δ is the angular distance d/R;
+         * d being the distance travelled, R the earth’s radius*
+         **/
+        let
+            radius = 6371e3, // meters
+            δ = Number(distance) / radius, // angular distance in radians
+            θ = gis.toRad(Number(bearing));
+        let φ1 = gis.toRad(coord[0]),
+            λ1 = gis.toRad(coord[1]);
+
+        let φ2 = Math.asin(Math.sin(φ1) * Math.cos(δ) +
+            Math.cos(φ1) * Math.sin(δ) * Math.cos(θ));
+
+        let λ2 = λ1 + Math.atan2(Math.sin(θ) * Math.sin(δ) * Math.cos(φ1),
+            Math.cos(δ) - Math.sin(φ1) * Math.sin(φ2));
+        // normalise to -180..+180°
+        λ2 = (λ2 + 3 * Math.PI) % (2 * Math.PI) - Math.PI;
+
+        return [gis.toDeg(φ2), gis.toDeg(λ2)];
+    },
     /**
      * All coordinates expected EPSG:4326
      * @param {Array} start Expected [lon, lat]
@@ -73,6 +97,23 @@ export let gis = {
             startLong = gis.toRad(start[0]),
             endLat = gis.toRad(end[1]),
             endLong = gis.toRad(end[0]),
+            dLong = endLong - startLong;
+
+        let dPhi = Math.log(Math.tan(endLat / 2.0 + Math.PI / 4.0) /
+            Math.tan(startLat / 2.0 + Math.PI / 4.0));
+
+        if (Math.abs(dLong) > Math.PI) {
+            dLong = (dLong > 0.0) ? -(2.0 * Math.PI - dLong) : (2.0 * Math.PI + dLong);
+        }
+
+        return (gis.toDeg(Math.atan2(dLong, dPhi)) + 360.0) % 360.0;
+    },
+    getBearingLatLon: function (start, end) {
+        let
+            startLat = gis.toRad(start[0]),
+            startLong = gis.toRad(start[1]),
+            endLat = gis.toRad(end[0]),
+            endLong = gis.toRad(end[1]),
             dLong = endLong - startLong;
 
         let dPhi = Math.log(Math.tan(endLat / 2.0 + Math.PI / 4.0) /
